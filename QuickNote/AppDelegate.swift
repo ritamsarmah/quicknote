1f3ccd156a40065d8afd47eaf15d036618f936af
ritamsarmah <sarmah.ritam@gmail.com>
2020-04-04 09:57:18 -0700

    Initial Commit

---
+++ QuickNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickNote
+//
+//  Created by Ritam Sarmah on 4/4/20.
+//  Copyright © 2020 Ritam Sarmah. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuickNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuickNote
+//
+//  Created by Ritam Sarmah on 4/4/20.
+//  Copyright © 2020 Ritam Sarmah. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
